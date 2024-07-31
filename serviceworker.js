d356ed025aa45b62ea9b03974e57e1285abe9339
Robert Michael <128392825+Robertmichaelavila@users.noreply.github.com>
2024-07-31 08:47:21 -0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-var staticCacheName = "pwa";
+var staticCacheName = "pwa_dice";
 
 const filesToCache = [
 	'./index.html',
@@ -7,21 +7,22 @@ const filesToCache = [
   './script.js',
 	'./image1-removebg-preview.png',
 ];
-
+ 
 self.addEventListener("install", function (e) {
-  e.waitUntil (
-    caches.open(staticCacheName).then(function (cache){
-      return cache.addAll(["/"]);
-    })
-  );
-})
-
+	e.waitUntil(
+		caches.open(staticCacheName).then(function (cache) {
+			console.log('ios');
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+ 
 self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
-
-  event.respondWith(
-    caches.match(event.request).then(function (response){
-      return response ?? fetch(event.request);
-    });
-  );
+	console.log(event.request.url);
+ 
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
+		})
+	);
 });
